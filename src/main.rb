64899cf392eb109284bce7e4e462940df17b7e04
Dotan Nahum <jondotan@gmail.com>
2020-01-19 18:10:24 +0200

    ii

---
+++ src/main.rb
@@ -0,0 +1,6 @@
+# The most common example of what scanners
+# look for: AWS keys. Spectral understands 100+ different
+# apps, with complex structures and adding more every
+# new version.
+BAD_NO_NO = "AKIAJCPPM3U66IKCPPCPPC"
+puts "It's no fun if you don't scan"
